266b7c03f14799a8e7f6dd5e0cb279a8bee508a0
THXRION666 <strike201420141@gmail.com>
2022-10-11 04:42:06 +0300

    added debug tools and implemented basic logic

--- lucky-charm.lua
+++ lucky-charm.lua
@@ -4,12 +4,11 @@ local BONES = nil
 local WEAPON_DATA = nil
 local PLAYER_IN_SCOPE_CAMERA_MODES = nil
 
-local settings = {
-}
-
 local ffi = require("ffi")
 local memory = require("memory")
 local samp_events = require("samp.events")
+
+local settings = nil
 
 local compute_3d_person_mouse_target = ffi.cast("void (__thiscall*)(void*, float, float, float, float, float*, float*)", 0x514970)
 local get_bone_position = ffi.cast("int (__thiscall*)(void*, float*, int, bool)", 0x5E4280)
@@ -40,58 +39,124 @@ local function get_bone_position_3d(ped, bone_id)
   return vec[0], vec[1], vec[2]
 end
 
-local function foreach_potential_target(callback)
-  local all_chars = getAllChars()
-
-  for _, potential_target in ipairs(all_chars) do
-    if potential_target == PLAYER_PED then
-      goto next
+local function foreach(array, callback)
+  for k, v in ipairs(array) do
+    local result = callback(v, k)
+    if result then
+      return result
     end
-
-    if isCharDead(potential_target) then
-      goto next
-    end
-    
-    local is_not_npc, _ = sampGetPlayerIdByCharHandle(potential_target)
-    if not is_not_npc then
-      goto next
-    end
-    
-    if callback(potential_target) then
-      return
-    end
-
-    ::next::
   end
 end
 
-local function foreach_valid_driver(callback)
-  foreach_potential_target(function(potential_target)
-    if not isCharInAnyCar(potential_target) then
+local function foreach_potential_target(callback)
+  local local_x, local_y, local_z = getCharCoordinates(PLAYER_PED)
+  local _, local_player_id = sampGetPlayerIdByCharHandle(PLAYER_PED)
+
+  local weapon_data = WEAPON_DATA[getCurrentCharWeapon(PLAYER_PED)]
+  if not weapon_data then
+    return
+  end
+
+  return foreach(getAllChars(), function(potential_target)
+    if potential_target == PLAYER_PED then
       return
+    end
+
+    if isCharDead(potential_target) then
+      return
+    end
+
+    local is_not_npc, potential_target_id = sampGetPlayerIdByCharHandle(potential_target)
+    if not is_not_npc then
+      return
+    end
+    if not isCharOnScreen(potential_target) then
+      return
+    end
+    
+    if settings.color_filter and sampGetPlayerColor(potential_target_id) ~= sampGetPlayerColor(local_player_id) then
+      return
+    end
+
+    if settings.range_filter and getDistanceBetweenCoords3d(local_x, local_y, local_z, getCharCoordinates(potential_target)) > weapon_data.max_range then
+      return
+    end
+
+    local result = callback(potential_target)
+    if result then
+      return result
     end
   end)
 end
 
---[[
-if color_filter && sampGetPlayerColor(potential_target_id) == sampGetPlayerColor(local_player_id) then
-  goto next
+local function foreach_valid_driver(callback)
+  local crosshair_x, crosshair_y = get_crosshair_position_2d()
+  local camera_x, camera_y, camera_z = getActiveCameraCoordinates()
+  local weapon_data = WEAPON_DATA[getCurrentCharWeapon(PLAYER_PED)]
+  if not weapon_data then
+    return
+  end
+
+  return foreach_potential_target(function(potential_target)
+    if not isCharInAnyCar(potential_target) then
+      return
+    end
+
+    local right_eye_x, right_eye_y, right_eye_z = get_bone_position_3d(ped, 6)
+    local left_eye_x, left_eye_y, left_eye_z = get_bone_position_3d(ped, 7)
+  
+    local head_x = (right_eye_x + left_eye_x) / 2
+    local head_y = (right_eye_y + left_eye_y) / 2
+    local head_z = (right_eye_z + left_eye_z) / 2
+
+    if not isLineOfSightClear(camera_x, camera_y, camera_z, head_x, head_y, head_z, settings.wall_filter, false, false, settings.wall_filter, false) then
+      return
+    end
+
+    local head_on_screen_x, head_on_screen_y = convert3DCoordsToScreen(head_x, head_y, head_z)
+    if getDistanceBetweenCoords2d(head_on_screen_x, head_on_screen_y, crosshair_x, crosshair_y) > settings.radius[weapon_data.type] then
+      return
+    end
+
+    local result = callback(potential_target)
+    if result then
+      return result
+    end
+  end)
 end
 
-local is_los_clear = isLineOfSightClear(
-  getActiveCameraCoordinates(),
-  x, y, z,
-  wall_filter,
-  wall_filter,
-  false,
-  wall_filter,
-  false
-)
+local function foreach_valid_pedestrian(callback)
+  local crosshair_x, crosshair_y = get_crosshair_position_2d()
+  local camera_x, camera_y, camera_z = getActiveCameraCoordinates()
+  local weapon_data = WEAPON_DATA[getCurrentCharWeapon(PLAYER_PED)]
+  if not weapon_data then
+    return
+  end
 
-if not is_los_clear then
-  goto next
+  return foreach_potential_target(function(potential_target)
+    if not isCharInAnyCar(potential_target) then
+      return
+    end
+
+    local bone = foreach(settings.bones, function(bone)
+      local bone_x, bone_y, bone_z = get_bone_position_3d(potential_target, bone)
+      if not isLineOfSightClear(camera_x, camera_y, camera_z, bone_x, bone_y, bone_z, settings.wall_filter, settings.wall_filter, false, settings.wall_filter, false) then
+        return
+      end
+      
+      local bone_on_screen_x, bone_on_screen_y = convert3DCoordsToScreen(head_x, head_y, head_z)
+      if getDistanceBetweenCoords2d(bone_on_screen_x, bone_on_screen_y, crosshair_x, crosshair_y) > settings.radius[weapon_data.type] then
+        return
+      end
+
+      return bone
+    end)
+
+    if bone then
+      return potential_target
+    end
+  end)
 end
-]]
 
 local function on_before_init()
   math.randomseed(os.time())
@@ -100,25 +165,49 @@ end
 local function on_init()
 end
 
+local function render_debug_sphere(x, y, z, radius)
+  local quarter_of_pi = math.pi / 4
+  local angles = {}
+  for i = 0, 7 do
+    table.insert(angles, quarter_of_pi * i)
+  end
+
+  for _, horizontal_angle in ipairs(angles) do
+    for _, vertical_angle in ipairs(angles) do
+      local offset_x = radius * math.sin(vertical_angle) * math.cos(horizontal_angle)
+      local offset_y = radius * math.sin(vertical_angle) * math.sin(horizontal_angle)
+      local offset_z = radius * math.cos(vertical_angle)
+    
+      local screen_x, screen_y = convert3DCoordsToScreen(x + offset_x, y + offset_y, z + offset_z)
+      renderDrawPolygon(screen_x, screen_y, 4, 4, 4, 0, 0x50FFFFFF)
+    end
+  end
+end
+
+local function render_debug_radius()
+  local crosshair_x, crosshair_y = get_crosshair_position_2d()
+  local weapon_data = WEAPON_DATA[getCurrentCharWeapon(PLAYER_PED)]
+  if not weapon_data then
+    return
+  end
+
+  local diameter = settings.radius[weapon_data.type] * 2
+  renderDrawPolygon(crosshair_x, crosshair_y, diameter, diameter, 30, 0, 0x50FFFFFF)
+end
+
 local function on_every_frame()
   foreach_potential_target(function(potential_target)
     do
-      local bone_x, bone_y = convert3DCoordsToScreen(get_bone_position_3d(potential_target, 8))
-      renderDrawPolygon(bone_x, bone_y, 4, 4, 4, 0, 0xFFFFFFFF)
+      local bone_x, bone_y, bone_z = get_bone_position_3d(potential_target, 3)
+      render_debug_sphere(bone_x, bone_y, bone_z, 0.04)
     end
-    do
-      local bone_x, bone_y = convert3DCoordsToScreen(get_bone_position_3d(potential_target, 5))
-      renderDrawPolygon(bone_x, bone_y, 4, 4, 4, 0, 0xFFFFFFFF)
-    end
-    return false
   end)
   
   if not settings.draw_radius then
     return
   end
 
-  local crosshair_x, crosshair_y = get_crosshair_position_2d()
-  renderDrawPolygon(crosshair_x, crosshair_y, settings.radius.HANDGUN, settings.radius.HANDGUN, 24, 0, 0x8000C000)
+  render_debug_radius()
 end
 
 local function on_send_bullet_sync()
@@ -139,25 +228,39 @@ function samp_events.onSendBulletSync()
   on_send_bullet_sync()
 end
 
-
-local function create_weapon_data_entry(type, range, damage)
-  return { type = type, range = range, damage = damage }
-end
-
 settings = {
   radius = {
-    HANDGUN = 30,
+    HANDGUN = 120,
     SUBMACHINE = 15,
     SHOTGUN = 30,
     RIFLE = 15,
   },
+  bones = {
+    3, 5, 7, 8, 201, 42, 52,
+  },
   chance = 35,
   color_filter = false,
   wall_filter = true,
-  distance_filter = true,
+  range_filter = true,
   driver_filter = false,
   focus_drivers = true,
   draw_radius = true,
+}
+
+BONES = {
+  [3] = "Spine",
+  [5] = "Neck",
+  [7] = "Right eye",
+  [8] = "Left eye",
+  [22] = "Right shoulder",
+  [32] = "Left shoulder",
+  [23] = "Right elbow",
+  [33] = "Left elbow",
+  [301] = "Right tit",
+  [302] = "Left tit",
+  [201] = "Belly",
+  [42] = "Left knee",
+  [52] = "Right knee",
 }
 
 PLAYER_IN_SCOPE_CAMERA_MODES = {
@@ -166,10 +269,10 @@ PLAYER_IN_SCOPE_CAMERA_MODES = {
   [46] = "MODE_CAMERA",
   [51] = "MODE_ROCKETLAUNCHER_HS",
 }
-BONES = {
-  [31] = "Chest",
-  [5] = "Head",
-}
+
+local function create_weapon_data_entry(type, max_range, damage)
+  return { type = type, max_range = max_range, damage = damage }
+end
 WEAPON_DATA = {
   [22] = create_weapon_data_entry("HANDGUN", 35.0, 8.25),
   [23] = create_weapon_data_entry("HADNGUN", 35.0, 13.2),
